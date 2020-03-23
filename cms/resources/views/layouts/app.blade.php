f5ce2d7a8742aa9ceb9aaa64cb4635c48d9e92b3
El attari Yassine <yassineattari64@gmail.com>
2020-03-23 15:05:52 +0100

    [CMS] Added models & migrations for post & category and auth directive for authenticated users only

--- cms/resources/views/layouts/app.blade.php
+++ cms/resources/views/layouts/app.blade.php
@@ -71,10 +71,31 @@
                 </div>
             </div>
         </nav>
-
+        @auth
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 py-4">
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <a href="/posts">Posts</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="/settings">Settings</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-8 ">
+                    <main class="py-4">
+                        @yield('content')
+                    </main>
+                </div>
+            </div>
+        </div>
+        @else
         <main class="py-4">
             @yield('content')
         </main>
+        @endauth
     </div>
 </body>
 </html>
