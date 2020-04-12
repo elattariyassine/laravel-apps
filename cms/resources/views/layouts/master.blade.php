373f01f2ccfde78674443e16dafadd96c6a62689
El attari Yassine <yassineattari64@gmail.com>
2020-04-12 00:34:54 +0000

    [IC] Config db & auth & navbar

--- cms/resources/views/layouts/master.blade.php
+++ cms/resources/views/layouts/master.blade.php
@@ -9,19 +9,15 @@
 
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
-    
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
-
     <link rel="stylesheet" href="css/aos.css">
-
     <link rel="stylesheet" href="css/ionicons.min.css">
-    
     <link rel="stylesheet" href="css/flaticon.css">
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
-    {{-- <link rel="stylesheet" href="{{ mix('/css/app.css') }}"> --}}
+    {{-- <link rel="stylesheet" href="{{ mix('/css/onetheme.css') }}"> --}}
   </head>
   <body>
     
--- cms/webpack.mix.js
+++ cms/webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+mix.styles(['public/css/style.css', 'public/css/icomoon.css', 'public/css/flaticon.css',
+    'public/css/ionicons.min.css', 'public/css/aos.css', 'public/css/magnific-popup.css', 'public/css/wl.theme.default.min.css', 'public/css/owl.carousel.min.css', 'public/css/animate.css.css', 'public/css/open-iconic-bootstrap.min.css'
+], 'public/css/onetheme.css')
